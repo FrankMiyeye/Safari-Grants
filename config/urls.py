ea2f9609f75632b100468ab3581eec5f1ea04c5b
Frank Miyeye <frankmiyeye99@gmail.com>
2026-06-26 00:03:37 +0300

    Opportunities page added

--- config/urls.py
+++ config/urls.py
@@ -4,13 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
 
-# Temporary home view
+# ======= Temporary Views =======
 def home(request):
     return render(request, 'home.html')
+
+def opportunities(request):
+    return render(request, 'opportunities.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('opportunities/', opportunities, name='opportunities'),
 ]
 
 if settings.DEBUG:
